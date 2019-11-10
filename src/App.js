0ffbb0317c3580d44efcbf19d25dd057dae553b5
Komei Yamaguchi <ymgc1994@icloud.com>
2019-11-10 11:55:07 +0900

    update:lint追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Todos from './components/Todos'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash'
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife'
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss'
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
